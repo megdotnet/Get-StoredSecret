f1590fce0e6d6d743bb9cd3b2a6299ad71fd7a08
Meghan Fairbanks <41306593+megdotnet@users.noreply.github.com>
2024-12-04 14:23:21 -0600

    Initial commit.

---
+++ config.ps1
@@ -0,0 +1,9 @@
+# This is a template.
+# Please supply your own values.
+
+#variables for mail message
+$from = "sender@domain[.]com"
+$to = @("recipient@domain[.]com")
+$smtpserver = "smtp.domain[.]com"
+$port = 587
+$smtpcred = Get-Credential
